da184a15f99104212aaeac2d350c2f1b2d2fd0d7
Abhijith T <abhijiththikkal@gmail.com>
2026-02-14 12:41:04 +0530

    Fixed jobs API for production

--- config.py
+++ config.py
@@ -1,18 +1,16 @@
 import os
 import mysql.connector
 
-
 def get_db_connection():
     try:
         connection = mysql.connector.connect(
-            host=os.environ.get("MYSQLHOST"),
-            user=os.environ.get("MYSQLUSER"),
-            password=os.environ.get("MYSQLPASSWORD"),
-            database=os.environ.get("MYSQLDATABASE"),
+            host=os.environ.get("MYSQLHOST", "localhost"),
+            user=os.environ.get("MYSQLUSER", "root"),
+            password=os.environ.get("MYSQLPASSWORD", "root123"),
+            database=os.environ.get("MYSQLDATABASE", "myedupath"),
             port=int(os.environ.get("MYSQLPORT", 3306))
         )
         return connection
-
     except mysql.connector.Error as err:
         print("Database connection failed:", err)
         return None
